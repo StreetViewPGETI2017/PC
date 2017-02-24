8517be6227bc28916272093ddf85c781e5d418b6
KujawskiCezary <kujawskicezary@wp.pl>
2017-02-24 21:59:06 +0100

    test

--- gui.py
+++ gui.py
@@ -1,3 +1,43 @@
 from urllib.request import urlopen
-html = urlopen("http://onet.pl")
-print(html.read())
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication
+
+class GUI(QWidget):
+
+    __STATIC_ADDRESS = "http://www.onet.pl"
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('StreetView')
+        self.show()
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
+        if e.key() == Qt.Key_W:
+            print("W")
+            print(GUI.ping(self,""))
+        if e.key() == Qt.Key_S:
+            print("S")
+        if e.key() == Qt.Key_A:
+            print("A")
+        if e.key() == Qt.Key_D:
+            print("D")
+        if e.key() == Qt.Key_Space:
+            print("Space")
+
+    def ping(self, command):
+        html = urlopen(self.__STATIC_ADDRESS + command)
+
+        return html.read()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = GUI()
+    sys.exit(app.exec_())
