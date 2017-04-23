af644f9df7097fbe5296347e5f5a92e9655fc953
KarKrol <krol_k_5@wp.pl>
2017-04-23 18:44:01 +0200

    sklejanie: działa i jest szybkie + komentarze

--- panorma.py
+++ panorma.py
@@ -1,17 +1,10 @@
 import numpy as np
 import cv2
 
-def stitch():
-    a = cv2.imread("0.jpg")
-    b = cv2.imread("1.jpg")
-    c = cv2.imread("2.jpg")
-    d = cv2.imread("3.jpg")
-    e = cv2.imread("4.jpg")
-    f = cv2.imread("5.jpg")
-    g = cv2.imread("6.jpg")
-    h = cv2.imread("7.jpg")
-    i = cv2.imread("8.jpg")
+def stitch(images, f):
 
+    a, b, c, d, e, f, g, h, i = images
+    #przycinanie zdjęć
     a = a[:,0.1296296296 * a.shape[1]:0.8703703704 * a.shape[1]]
     b = b[:,0.1296296296 * b.shape[1]:0.8703703704 * b.shape[1]]
     c = c[:,0.1296296296 * c.shape[1]:0.8703703704 * c.shape[1]]
@@ -21,7 +14,7 @@ def stitch():
     g = g[:,0.1296296296 * g.shape[1]:0.8703703704 * g.shape[1]]
     h = h[:,0.1296296296 * h.shape[1]:0.8703703704 * h.shape[1]]
     i = i[:,0.1296296296 * i.shape[1]:0.8703703704 * i.shape[1]]
-
+    #sklejanie zdjęć
     result = np.concatenate((a, b), axis=1)
     result = np.concatenate((result, c), axis=1)
     result = np.concatenate((result, d), axis=1)
@@ -31,7 +24,21 @@ def stitch():
     result = np.concatenate((result, h), axis=1)
     result = np.concatenate((result, i), axis=1)
 
-    cv2.imwrite("cos.jpg", result)
+    #cv2.imwrite("p_" + f + ".jpg", result)
+    return result
 
 
+#do prezentacji
+a = cv2.imread("0.jpg")
+b = cv2.imread("1.jpg")
+c = cv2.imread("2.jpg")
+d = cv2.imread("3.jpg")
+e = cv2.imread("4.jpg")
+f = cv2.imread("5.jpg")
+g = cv2.imread("6.jpg")
+h = cv2.imread("7.jpg")
+i = cv2.imread("8.jpg")
 
+images = [a, b, c, d, e, f, g, h, i]
+result = stitch(images, 2)
+cv2.imwrite("result.jpg", result)
